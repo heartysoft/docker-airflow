4fdd1ebeb194b53ba90a974dbe7a9cc5c8eaae7c
ashic <ashic@live.com>
2017-06-25 00:56:48 +0100

    added travis build

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -xe
+
+export REPO=${REPO:-heartysoft/docker-airflow}
+export TAG_TO_USE=${TRAVIS_TAG:-snapshot}
+docker build -t $REPO:latest .
+docker tag $REPO:latest $REPO:$TAG_TO_USE
+#docker build -t $REPO:latest-helm -f DockerfileHelm .
+#docker tag $REPO:latest-helm $REPO:$TAG_TO_USE-helm
