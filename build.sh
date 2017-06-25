bc4b968e1e600e638f20b6c564fd54b08979145f
ashic <ashic@live.com>
2017-06-25 01:18:35 +0100

    adding helm support

--- build.sh
+++ build.sh
@@ -6,5 +6,5 @@ export REPO=${REPO:-heartysoft/docker-airflow}
 export TAG_TO_USE=${TRAVIS_TAG:-snapshot}
 docker build -t $REPO:latest .
 docker tag $REPO:latest $REPO:$TAG_TO_USE
-#docker build -t $REPO:latest-helm -f DockerfileHelm .
-#docker tag $REPO:latest-helm $REPO:$TAG_TO_USE-helm
+docker build -t $REPO:latest-helm -f DockerfileHelm .
+docker tag $REPO:latest-helm $REPO:$TAG_TO_USE-helm
