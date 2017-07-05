ed23979dc07b358dd068d115a5ccd8c96a4536d4
ashic <ashic@live.com>
2017-07-05 14:38:48 +0100

    added ability to specify a requirements.txt file which will be loaded on startup. Note: run time, not build time.

--- start.sh
+++ start.sh
@@ -3,6 +3,10 @@
 export LOGGING_LEVEL=${LOGGING_LEVEL:-WARN}
 sed -i "s/LOGGING_LEVEL =.*$/LOGGING_LEVEL = logging.$LOGGING_LEVEL/g" /opt/conda/lib/python3.6/site-packages/airflow/settings.py 
 
+if [ -f /app/requirements.txt ]; then
+  pip install -r /app/requirements.txt
+fi
+
 n=0
 until [ $n -ge 3 ]
 do
